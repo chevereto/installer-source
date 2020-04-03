413095f050dc678595130d43d7308d9053a59880
Rodolfo Berrios <inbox@rodolfoberrios.com>
2020-04-03 14:27:58 -0300

    Updated nginx rules

--- template/nginx.php
+++ template/nginx.php
@@ -1,28 +1,33 @@
 <?php
 
 header('Content-Type: text/plain');
-printf('# Chevereto nginx generated rules for ' . $runtime->rootUrl . '
-## Disable access to sensitive files
+printf('# Chevereto NGINX generated rules for ' . $runtime->rootUrl . '
+
+# Context limits
+client_max_body_size 20M;
+
+# Disable access to sensitive files
 location ~* ' . $runtime->relPath . '(app|content|lib)/.*\.(po|php|lock|sql)$ {
   deny all;
 }
-## CORS headers
-location ~* ' . $runtime->relPath . '.*\.(ttf|ttc|otf|eot|woff|woff2|font.css|css|js) {
+
+# Image not found replacement
+location ~ \.(jpe?g|png|gif|webp)$ {
+    log_not_found off;
+    error_page 404 ' . $runtime->relPath . 'content/images/system/default/404.gif;
+}
+
+# CORS header (avoids font rendering issues)
+location ~* ' . $runtime->relPath . '.*\.(ttf|ttc|otf|eot|woff|woff2|font.css|css|js)$ {
   add_header Access-Control-Allow-Origin "*";
 }
-## Upload path for image content only and set 404 replacement
-location ^~ ' . $runtime->relPath . 'images/ {
-  location ~* (jpe?g|png|gif) {
-      log_not_found off;
-      error_page 404 ' . $runtime->relPath . 'content/images/system/default/404.gif;
-  }
-  return 403;
-}
-## Pretty URLs
+
+# Pretty URLs
 location ' . $runtime->relPath . ' {
   index index.php;
-  try_files $uri $uri/ ' . $runtime->relPath . 'index.php?$query_string;
+  try_files $uri $uri/ /index.php$is_args$query_string;
 }
-# END Chevereto nginx rules
+
+# END Chevereto NGINX rules
 ');
 die();
